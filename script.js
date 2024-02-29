b090b2457329f6ed1fac31823add4a290bfd487c
Mehrob <your_email@example.com>
2024-02-29 19:02:04 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+//1
+// for(let i = 1; i < 101; i++){
+//     console.log(i);
+// }
+//2
+// for(let i = -100;i < 0; i++){
+//     console.log(i)
+// }
+//3
+// for(let i = 100;i > 0;i--){
+//     console.log(i);
+// }
+//4
+// for(let i = 1;i <= 100;i++){
+//     if(i % 2 == 0){
+//         console.log(i)
+//     }
+// }
+//5
+// for(let i = 1; i <= 100; i++){
+//     if(i % 3 == 0){
+//         console.log(i)
+//     }
+// }
+//6
+// let arr = []
+// for(let i = 1;i <= 10; i++){
+//     arr.push(i);
+// }
+//7
+// let arr = []
+// for(let i = 1;i <= 100; i++){
+//     if(i % 2 == 0){
+//         arr.push(i);
+//     }
+// }
+// console.log(arr)
+//8
+// const arr = [1,3,-1,-5,2,-2,4,-3];
+// let res = 0;
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] < 0){
+//         res++
+//     }
+// }
+// console.log(res)
+//9
+// const arr = [1,3,-1,-5,2,-2,4,-3,2];
+// let res = 0;
+// for(let i = 0;i < arr.length;i++){
+//     if(arr[i] > 0){
+//         res++
+//     }
+// }
+// console.log(res);
+// let str = "hello";
+// if (str.length >= 2) {
+//     str = str.slice(0, -2) + str[4];
+// }
+// console.log(str);
